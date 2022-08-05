78dcb03ac97aa017ef920ad37f89568d85fb788e
Melissa Alvarez <softwareprojectsmeli@gmail.com>
2022-08-04 23:18:29 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_appium_driver_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_appium_driver_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test_driver/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_appium_driver_example/main.dart' as app; 
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+    enableFlutterDriverExtension();
+
+  app.main();
+}
